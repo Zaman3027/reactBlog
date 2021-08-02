0c45e6639dbca86c6b84bf3270190d8ee7529079
zaman3027 <mahafuzzaman02@gmail.com>
2021-08-02 17:24:09 +0530

    heroku

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,3 @@
 
-const proURL = process.env.REACT_APP_DEV_MODE ? 'http://localhost:5000/' : 'https://young-everglades-08176.herokuapp.com/';
+const proURL = 'https://young-everglades-08176.herokuapp.com/';
 export { proURL };
